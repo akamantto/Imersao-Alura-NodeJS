8ae040a269c1d17c7b2bf63a1abed0b7698f7984
pedro <pedrohenriqueabrantes.ph@gmail.com>
2024-11-20 17:52:20 -0300

    modularizaçao

--- server.js
+++ server.js
@@ -1,48 +1,10 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "Gato fofo dormindo",
-        imagem: "https://placekitten.com/400/300"
-    },
-    {
-        id: 3,
-        descricao: "Gato Coitado",
-        imagem: "https://placecats.com/millie/300/150"
-    }
-];
+import routes from "./src/routes/postsRoutes.js";
+
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(3000, () => {
-    console.log("servidor escutando");
+    console.log("Servidor escutando...");
 });
-
-async function getTodosPosts(){
-    const db = conexao.db("imersao-Instabyte");
-    const colecao = db.collection("posts");
-    return colecao.find().toArray();
-}
-
-app.get("/posts", async (req, res) => {
-    const posts = await getTodosPosts();
-    res.status(200).json(posts);
-});
-
-// function buscarPostPorID(id) {
-//     return posts.findIndex((post) => {
-//         return post.id === Number(id)
-//     })
-// }
-
-// app.get("/posts/:id", (req, res) => {
-//     const index = buscarPostPorID(req.params.id);
-//     res.status(200).json(posts[index]);
-// });
